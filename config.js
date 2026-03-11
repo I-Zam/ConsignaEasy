2e1a2bcf32e79325c05090b3a38a6eb74b11865a
I-Zam <78179226+I-Zam@users.noreply.github.com>
2026-03-11 16:57:09 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,9 @@
+// ============================================
+// CONSIGNAEASY - CONFIGURATION
+// ============================================
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwlfX_q00JhPXa29V6uMvXO5FBCaDB11rAvwdryJm1NxowMlCmymZefB6aTe7XdwLko/exec";
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbw6RXmDrEinziBma1BTNJYklyLdcch8Xmc80zCv3uEDkKPs1omfNmkxcgKaJtfK1r9M/exec'
+};
+
+const API_URL = CONFIG.API_URL;
